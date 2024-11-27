8af0a4c0fd883091136f3ed31ab9fda3d908faf8
Ayush Sharma <ayushsharma4457@gmail.com>
2024-11-27 16:53:34 +0530

    First Code And Data Type
    Hello World And Data Types

---
+++ Code/Data Types/data_types.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int num = 10;
+    float dec = 4.567;
+    char c = 'a';
+    bool is = true;
+    double deci = 3.463756;
+    long int numb = 464363878;
+
+    cout << "Integer: " << num << endl;
+    cout << "Decimal: " << dec << endl;
+    cout << "Character: " << c << endl;
+    cout << "Boolean: " << is << endl;
+    cout << "Double: " << deci << endl;
+    cout << "Long: " << numb << endl;
+
+    return 0;
+}
