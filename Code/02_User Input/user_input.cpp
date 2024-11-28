f32b8482b54478d1fc6cb81d0f5a7c080165e2a7
Ayush Sharma <ayushsharma4457@gmail.com>
2024-11-28 21:26:52 +0530

    User Input & TypeCasting
    Write Code Of User Input & TypeCasting.

---
+++ Code/02_User Input/user_input.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b;
+
+    cout << "Enter The Number: ";
+    cin >> a >> b;
+
+    cout << "Addition: " << a + b;
+
+    return 0;
+}
